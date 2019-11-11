4f993ca4f3e7f22690755d62643937a618119570
Naya  Willis <greedybrain18@gmail.com>
2019-11-11 03:35:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,9 +53,9 @@ class Student
   end
   
   def self.new_from_db(row) 
+    binding.pry
     new_student = Student.new
     new_student.id = row[0]
-    binding.pry
     new_student.name = row[1]
     new_student.grade = row[2]
     new_student
