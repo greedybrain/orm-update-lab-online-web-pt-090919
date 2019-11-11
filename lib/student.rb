a0a8a8dd4a704d789cc71e4d6976b3e7c3e12bbc
Naya  Willis <greedybrain18@gmail.com>
2019-11-11 03:05:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ require_relative "../config/environment.rb"
 
 class Student
   
-  attr_reader name, grade, id
+  attr_reader :name, :grade, :id
 
   def initialize(name, grade, id=nil)
     @name = name
